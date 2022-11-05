528007fc17fa0158bd6ca21e92be34485c39c957
Alex Robinson <arobinson@conceptsystemsinc.com>
2022-11-05 11:53:14 -0700

    finish findItem function

--- libs/turtleExt.lua
+++ libs/turtleExt.lua
@@ -2,8 +2,17 @@ local function findItem(name)
   -- Loop through each inventory slot until the named item is found
   local numSlots = 16
 
+  -- Format search term for backend
+  local needle = string.gsub(string.lower(name), " ", "_")
+
   for i = 1,numSlots do
     local detail = turtle.getItemDetail(i)
+    local pos = string.find(detail.name, ":") + 1
+    local name = string.sub(detail.name, pos)
+
+    if (name == needle) then
+      return i
+    end
   end
 end
 
